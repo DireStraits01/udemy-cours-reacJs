f2a0f2805cb2d3281226f60dc893994f7794f190
DireStraits01 <arsamak88@gmail.com>
2023-02-26 22:19:50 +0300

    lesson-20

---
+++ S2/lesson-20/script.js
@@ -0,0 +1,12 @@
+'use strict';
+let num = 67;
+switch (num) {
+  case num < 49:
+    console.log('мало');
+    break;
+  case num > 100:
+    console.log('mnogo');
+    break;
+  default:
+    console.log('ok');
+}
